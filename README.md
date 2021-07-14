dd8161cad08b3114c26aa3f1d2f2875fb60fc921
Gergely Bihary <gergely.bihary@prezi.com>
2021-07-14 10:06:29 +0200

    Sync custom questions/answers to Mailchimp

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ MAILCHIMP_API_KEY=<API key here>
 MAILCHIMP_TAG=<The tag you want to add, ideally the event name>
 API_KEY=<A custom key for securing your API>
 PORT=<Server port, eg.: 8000>
+
+STATUS_MERGE_FIELD=<The merge field for Junction registration status in Mailchimp, like: JUNC_STAT>
+CUSTOM_ANSWERS_TO_SYNC=This is an array like string like this: <MERGE_FIELD>:<CUSTOM_QUESTION_ID>,<MERGE_FIELD2>:<CUSTOM_QUESTION_ID2>, eg.: JUNC_PREF:some-question
 ```
 
 ## Connect
--- src/mailchimp.ts
+++ src/mailchimp.ts
@@ -9,6 +9,7 @@ type RegistrationData = {
     firstName?: string;
     lastName?: string;
     email?: string;
+    CustomAnswers?: { section: string; key: string; value: any }[];
   };
 };
 
@@ -26,20 +27,42 @@ const sendToMailchimp = (method: Method, path: string, data: any) => {
     .catch((err) => console.log(err));
 };
 
+const getCustomMergeFields = (registrationData: RegistrationData) => {
+  const customAnswersToSync = (process.env.CUSTOM_ANSWERS_TO_SYNC || "").split(
+    ","
+  );
+
+  const customMergeFields: { [mergeField: string]: string } = {};
+  customAnswersToSync.forEach((customAnswerToSync) => {
+    const [mergeField, customAnswerId] = customAnswerToSync.split(":");
+    customMergeFields[mergeField] =
+      registrationData?.answers?.CustomAnswers?.find(
+        (a) => a.key === customAnswerId
+      )?.value;
+  }, {});
+
+  return customMergeFields;
+}
+
 const createOrUpdateContact = (
   hash: string,
   registrationData: RegistrationData
 ) => {
+  const statusMergeField = process.env.STATUS_MERGE_FIELD || "JUNC_STAT";
+
   const mailchimpData = {
     email_address: registrationData?.answers?.email,
     status_if_new: "subscribed",
     merge_fields: {
       FNAME: registrationData?.answers?.firstName,
       LNAME: registrationData?.answers?.lastName,
-      JUNC_STAT: registrationData.status,
+      [statusMergeField]: registrationData.status,
+      ...getCustomMergeFields(registrationData),
     },
     tags: [process.env.MAILCHIMP_TAG],
   };
+
+  console.log("Sending participant data to MailChimp...", mailchimpData);
 
   return sendToMailchimp(
     "PUT",
@@ -66,6 +89,7 @@ const updateContactTags = (hash: string) => {
 };
 
 export const onRegistration = async (registrationData: RegistrationData) => {
+  console.log(registrationData, registrationData?.answers?.CustomAnswers);
   console.log(
     `Syncing data with Mailchimp for e-mail: ${registrationData.answers?.email}`
   );
